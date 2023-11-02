022dc80fa1d091a46d20115fc6ce8b82c8503db2
bipinjoshi <bipinjoshicr712@gmail.com>
2023-11-02 14:31:45 +0530

    localstorage funtionality added in add to cart

--- src/component/constext/CartContext.js
+++ src/component/constext/CartContext.js
@@ -1,19 +1,35 @@
-import { createContext, useContext, useReducer } from "react";
+import { createContext, useContext, useEffect, useReducer } from "react";
 import reducer from "../Resucer/CartReducer";
 
+const getLocaldata = () => {
+  let localStorageDate = localStorage.getItem("localData");
+  console.log(localStorageDate);
+  if (localStorageDate) {
+    return JSON.parse(localStorage.getItem("localData"));
+  } else {
+    return [];
+  }
+};
 export const CartContext = createContext();
 const initialState = {
-  addCart: [],
+  // addCart: [],
+  addCart: getLocaldata(),
+
   total_item: "",
 };
 export const CartProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
+
   const addtocart = (id, product, color, amount) => {
     dispatch({ type: "ADD_CART", payload: { id, product, color, amount } });
   };
   const setRemoveItem = (id) => {
     dispatch({ type: "remove_item", payload: id });
   };
+
+  useEffect(() => {
+    localStorage.setItem("localData", JSON.stringify(state.addCart));
+  }, [state.addCart]);
   return (
     <CartContext.Provider value={{ ...state, addtocart, setRemoveItem }}>
       {children}
